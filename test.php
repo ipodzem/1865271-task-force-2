643c24ef16aaa74c7c54517c5fb5bddfdfb972f8
ipodzem <ipodzemelnaya@yandex.ru>
2022-01-13 10:49:49 +0600

    fix namespaces

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 require(__DIR__.'/vendor/autoload.php');
-use HtmlAcademy\services\Task;
+use Taskforce\services\Task;
 function assert_failure($file, $line, $assertion, $message)
 {
     echo "Проверка $assertion провалена: $message";
