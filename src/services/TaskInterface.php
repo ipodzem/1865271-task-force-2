d313c44757ed8aee15f5684a44544843e0f51ece
ipodzem <ipodzemelnaya@yandex.ru>
2022-01-19 09:58:58 +0600

    files from master

--- src/services/TaskInterface.php
+++ src/services/TaskInterface.php
@@ -9,11 +9,16 @@ interface TaskInterface
     const STATUS_FAIL    = 'fail';
     const STATUS_PROCESS = 'process';
     const STATUS_SUCCESS = 'success';
+    const STATUS_ARCHIVE = 'archive';
 
     const ACTION_CANCEL  = 'cancel_action';
     const ACTION_AGREE   = 'agree_action';
     const ACTION_SUCCESS = 'success_action';
     const ACTION_FAIL    = 'fail_action';
+    const ACTION_ARCHIVE = 'archive_action';
+
+    CONST TYPE_CUSTOMER  = 'customer';
+    CONST TYPE_EXECUTOR  = 'executor';
 
 
     public function __construct(int $executor_id, int $customer_id);
