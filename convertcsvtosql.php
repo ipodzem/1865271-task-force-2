2e779c94cb521a7ea9aa1c869e635ac862fbd8f0
ipodzem <ipodzemelnaya@yandex.ru>
2022-02-08 10:02:50 +0600

    fixes

--- convertcsvtosql.php
+++ convertcsvtosql.php
@@ -3,10 +3,24 @@
 require(__DIR__ . '/vendor/autoload.php');
 
 use Taskforce\services\ImportService;
-$path = realpath(dirname(__FILE__)).'/data/sql/';
+use Taskforce\datasets\CategoriesImport;
+use Taskforce\datasets\CitiesImport;
+use Taskforce\datasets\UsersImport;
+use Taskforce\datasets\TasksImport;
+use Taskforce\datasets\ProfilesImport;
+use Taskforce\datasets\RepliesImport;
+use Taskforce\datasets\RatingsImport;
+
+$path = realpath(dirname(__FILE__)) . '/data/sql/';
 
 try {
-    ImportService::generateSqlFiles($path);
+    (new ImportService(new UsersImport()))->saveTo($path . "users.sql");
+    (new ImportService(new TasksImport()))->saveTo($path . "tasks.sql");
+    (new ImportService(new CategoriesImport()))->saveTo($path . "cats.sql");
+    (new ImportService(new CitiesImport()))->saveTo($path . "cities.sql");
+    (new ImportService(new ProfilesImport()))->saveTo($path . "profiles.sql");
+    (new ImportService(new RepliesImport()))->saveTo($path . "replies.sql");
+    (new ImportService(new RatingsImport()))->saveTo($path . "ratings.sql");
     echo "Файлы сгенерированы в папку " . $path;
 } catch (Exception $e) {
     echo 'Ошибка генерации файлов: ', $e->getMessage(), "\n";
--- src/services/ImportService.php
+++ src/services/ImportService.php
@@ -2,59 +2,22 @@
 
 namespace Taskforce\services;
 
-use Taskforce\datasets\CategoriesImport;
-use Taskforce\datasets\CitiesImport;
-use Taskforce\datasets\UsersImport;
-use Taskforce\datasets\TasksImport;
-use Taskforce\datasets\ProfilesImport;
-use Taskforce\datasets\RepliesImport;
-use Taskforce\datasets\RatingsImport;
 use Taskforce\exceptions\BaseException;
 
 class ImportService
 {
-    public static function generateSqlFiles(string $path): void
-    {
-        $data = [
-            'users' => [
-                'model' => new UsersImport(),
-                'file' => 'users.sql'
-            ],
-            'cats' => [
-                'model' => new CategoriesImport(),
-                'file' => 'cats.sql'
-            ],
-            'cities' => [
-                'model' => new CitiesImport(),
-                'file' => 'cities.sql'
-            ],
-            'tasks' => [
-                'model' => new TasksImport(),
-                'file' => 'tasks.sql'
-            ],
-            'profiles' => [
-                'model' => new ProfilesImport(),
-                'file' => 'profiles.sql'
-            ],
-            'replies' => [
-                'model' => new RepliesImport,
-                'file' => 'replies.sql'
-            ],
-            'ratings' => [
-                'model' => new RatingsImport,
-                'file' => 'rate.sql'
-            ],
-        ];
+    private $model;
 
-        foreach ($data as $info) {
-            if (!ImportService::saveTo($info['model'], $info ['file'], $path)) {
-                throw BaseException('Problem with generating '.$info['file']);
-            }
-        }
-    }
-    private static function saveTo($model, string $file, string $path): bool
+    public function __construct($model)
     {
-        return (file_put_contents($path . $file, $model->sql()));
+        $this->model = $model;
+    }
+
+    public function saveTo(string $file): void
+    {
+        if (!file_put_contents($file, $this->model->sql())) {
+            throw new BaseException('Ошибка записи файла ' . $file);
+        }
     }
 
 }
