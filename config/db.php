5541b9ee75db93114adec5ac0df64f9ece8bbe39
ipodzem <ipodzemelnaya@yandex.ru>
2022-02-24 15:59:06 +0600

    Revert "models generation" Wrong branch This reverts commit 1b7c45c6b354597357298b41b848b496f768aa79.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=academy',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'secret',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
