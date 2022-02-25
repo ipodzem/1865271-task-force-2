1471340b1720fe5eef653c417e6634a48c6926b3
ipodzem <ipodzemelnaya@yandex.ru>
2022-02-25 10:44:24 +0600

    fix Singularize

---
+++ models/Rating.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ratings".
+ *
+ * @property int $id
+ * @property int $response_id
+ * @property int $rating
+ * @property string $comment
+ * @property string $created
+ *
+ * @property Response $response
+ */
+class Rating extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'ratings';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['response_id', 'rating', 'comment'], 'required'],
+            [['response_id', 'rating'], 'integer'],
+            [['comment'], 'string'],
+            [['created'], 'safe'],
+            [['response_id'], 'exist', 'skipOnError' => true, 'targetClass' => Response::className(), 'targetAttribute' => ['response_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'response_id' => 'Response ID',
+            'rating' => 'Rating',
+            'comment' => 'Comment',
+            'created' => 'Created',
+        ];
+    }
+
+    /**
+     * Gets query for [[Response]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getResponse()
+    {
+        return $this->hasOne(Response::className(), ['id' => 'response_id']);
+    }
+}
---
+++ models/Response.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "responses".
+ *
+ * @property int $id
+ * @property int $task_id
+ * @property int $user_id
+ * @property int $amount
+ * @property string $text
+ * @property string $created
+ *
+ * @property Rating[] $ratings
+ * @property Task $task
+ * @property User $user
+ */
+class Response extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'responses';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['task_id', 'user_id', 'amount', 'text', 'created'], 'required'],
+            [['task_id', 'user_id', 'amount'], 'integer'],
+            [['text'], 'string'],
+            [['created'], 'safe'],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Task::className(), 'targetAttribute' => ['task_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'task_id' => 'Task ID',
+            'user_id' => 'User ID',
+            'amount' => 'Amount',
+            'text' => 'Text',
+            'created' => 'Created',
+        ];
+    }
+
+    /**
+     * Gets query for [[Ratings]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRatings()
+    {
+        return $this->hasMany(Rating::className(), ['response_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Task::className(), ['id' => 'task_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
