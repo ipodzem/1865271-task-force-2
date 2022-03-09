f15787a35f1f23d68c225641edba51a8702928b4
ipodzem <ipodzemelnaya@yandex.ru>
2022-03-09 08:47:18 +0600

    tasks list with filter

--- views/tasks/index.php
+++ views/tasks/index.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\widgets\ListView;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -19,40 +20,40 @@ $this->params['breadcrumbs'][] = $this->title;
             'dataProvider' => $dataProvider,
             'layout' => '{items}',
             'itemOptions' => ['class' => 'item'],
-            'itemView' => '_task'
+            'itemView' => '_task',
+            'emptyText' => 'Нет новых задач'
         ]) ?>
     </div>
     <div class="right-column">
         <div class="right-card black">
             <div class="search-form">
-                <form>
-                    <h4 class="head-card">Категории</h4>
-                    <div class="form-group">
-                        <div>
-                            <input type="checkbox" id="сourier-services" checked>
-                            <label class="control-label" for="сourier-services">Курьерские услуги</label>
-                            <input id="cargo-transportation" type="checkbox">
-                            <label class="control-label" for="cargo-transportation">Грузоперевозки</label>
-                            <input id="translations" type="checkbox">
-                            <label class="control-label" for="translations">Переводы</label>
-                        </div>
-                    </div>
-                    <h4 class="head-card">Дополнительно</h4>
-                    <div class="form-group">
-                        <input id="without-performer" type="checkbox" checked>
-                        <label class="control-label" for="without-performer">Без исполнителя</label>
-                    </div>
-                    <h4 class="head-card">Период</h4>
-                    <div class="form-group">
-                        <label for="period-value"></label>
-                        <select id="period-value">
-                            <option>1 час</option>
-                            <option>12 часов</option>
-                            <option>24 часа</option>
-                        </select>
-                    </div>
-                    <input type="button" class="button button--blue" value="Искать">
-                </form>
+                <?php
+                $form = ActiveForm::begin([
+                    'id'      => 'filter-form',
+                    'method'  => 'GET',
+                    'action' => Url::to(['tasks/index']),
+                    'options' => ['class' => 'search-form']
+                ]); ?>
+                <h4 class="head-card">Категории</h4>
+                <div class="form-group">
+                    <?= $form->field($searchModel, 'category_ids')->checkboxList($categories,  ['unselect' => null])->label('') ?>
+                </div>
+                <h4 class="head-card">Дополнительно</h4>
+                <div class="form-group">
+                    <?= $form->field($searchModel, 'without_executor')->checkbox([
+                        'label' => 'Без исполнителя',
+                    ]) ?>
+                </div>
+                <h4 class="head-card">Период</h4>
+                <div class="form-group">
+                    <?= $form->field($searchModel, 'period')->dropDownList(
+                        ['' => '', '1' => '1 час', '12' => '12 часов', '24' => '24 часа']
+                    )->label('') ?>
+
+                </div>
+                <?= Html::submitButton('Искать', ['class' => 'button button--blue']) ?>
+                <?php
+                ActiveForm::end(); ?>
             </div>
         </div>
     </div>
